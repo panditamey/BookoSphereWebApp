331bfc985a0c0efb47bbf67bbb9ea08c77b7c023
panditamey <codewithamey@gmail.com>
2024-02-08 01:05:15 +0530

    Update book generation text

--- components/Home.tsx
+++ components/Home.tsx
@@ -21,7 +21,7 @@ function Home() {
                 </div>
                 <div className="flex flex-col items-center">
                     <img width={200} src="appbar.png" alt="" />
-                    <span className='font-bold'>Generate and Read Books of Your Own Choice</span>
+                    <span className='font-bold'>Read and Generate AI-Written Books of Your Own Choice.</span>
                     <br />
                     <span>Download the Android App now</span>
                     <Button className='mt-5 px-10 py-5 h-8 text-sm text-white rounded-full border-purple-100 border-solid border-2 bg-purple-500' onClick={
